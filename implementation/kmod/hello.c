0f61b0b8e4e7082f23d0316fe0a729f05c8fdf86
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2025-01-22 17:20:23 +0100

    Implementation/Kmod: Some more hello

--- implementation/kmod/hello.c
+++ implementation/kmod/hello.c
@@ -1,16 +1,24 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 
+void test(void);
+
+
 MODULE_LICENSE("Proprietary");
 
 int init_module(void)
 {
 	printk(KERN_INFO "Hello world 1.\n");
+	test();
 
 	/* 
 	 * A non 0 return means init_module failed; module can't be loaded. 
 	 */
 	return 0;
+}
+
+void test(void) {
+	pr_info("we just called a different function\n");
 }
 
 void cleanup_module(void)
