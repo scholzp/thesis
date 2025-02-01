2aba99e008ef2bd03365dcd37245e51048e0e92d
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2025-02-01 09:32:14 +0100

    sync

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@ in
 let
   kernel = pkgs.linuxPackages.kernel;
   lib = pkgs.lib;
+  tee_kernel = implementation/kernel.elf32;
   # Hardcode start address of reserved low memory.
   # Take care of page granularity, which means the address should be divisible by 4096.
   start_address = "0xc8000";
@@ -22,7 +23,7 @@ let
 
   stdenv = pkgs.stdenv;
   kmod = pkgs.callPackage ./nix/buildKmod.nix {inherit lib; inherit stdenv; inherit kernel; inherit start_address; inherit low_mem_size;};
-  initrd = pkgs.callPackage ./nix/initrd.nix {inherit lib; inherit pkgs; inherit kmod;};
+  initrd = pkgs.callPackage ./nix/initrd.nix {inherit lib; inherit pkgs; inherit kmod; inherit tee_kernel;};
   runQemu = pkgs.callPackage ./nix/run_qemu.nix {inherit kernel; inherit initrd; inherit start_address; inherit low_mem_size;};
 in {
   inherit initrd;
